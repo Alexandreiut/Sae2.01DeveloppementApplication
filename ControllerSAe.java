29d1004234ff638be439b4b3804231f59e954cad
Dupont Pierre <pierre.dupont@iut-rodez.fr>
2023-05-26 11:48:43 +0200

    ok

---
+++ ControllerSAe.java
@@ -0,0 +1,53 @@
+package iut.sae.application;
+
+
+
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class ControllerSAe {
+
+    @FXML
+    private ImageView smileID;
+
+    @FXML
+    private HBox bas;
+
+    @FXML
+    private GridPane JeuID;
+
+    @FXML
+    private Label MinuteurID;
+
+    @FXML
+    private Label DrapeauRestantsID;
+
+    @FXML
+    private HBox HautID;
+
+    @FXML
+    private VBox GaucheId;
+
+    @FXML
+    private VBox DroitId;
+
+    @FXML
+    private Label DifficultéID;
+
+    @FXML
+    void newGame(ActionEvent event) {
+    	
+    }
+
+    @FXML
+    void clickAction(ActionEvent event) {
+
+    }
+
+}
